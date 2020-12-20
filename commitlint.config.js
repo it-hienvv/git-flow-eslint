45b6c4ba4e1c3c9bb511b91577d5c1bb8377fbb2
it-hienvv <hienvukhmt93@gmail.com>
2020-12-20 12:52:34 +0700

    a(block-a): change block
    ABCXYZ

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,8 +2,8 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', ['a', 'b']],
-    'header-max-length': [2, 'always', 100],
-    'footer-leading-blank': [2, 'always', 100],
-    'scope-case': [2, 'always'],
+    'header-max-length': [2, 'always', 99],
+    'footer-leading-blank': [2, 'always', 99],
+    'scope-case': [2, 'always', 'lower-case'],
   },
 };
